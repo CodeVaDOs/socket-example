094c30c36868f7fe990a5b4227451c596d27f06e
Vadym <tvadost2003@gmail.com>
2021-08-07 14:33:58 +0300

    Init

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react';
+import {Client} from '@stomp/stompjs';
+import MessageCreator from "./components/MessageCreator";
+
+const SOCKET_URL = "ws://localhost:9000/ws-message";
+
+function App() {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const onConnected = () => {
+            console.log("Connected websocket")
+            console.log("Subscribe on /topic/message");
+            client.subscribe('/topic/message', function (msg) {
+                console.log("Received message: ", JSON.parse(msg.body))
+                if (msg.body) {
+                    const jsonBody = JSON.parse(msg.body);
+                    if (jsonBody.message) {
+                        setMessages(prev => [...prev, jsonBody.message])
+                    }
+                }
+            });
+        }
+
+        const onDisconnected = () => {
+            console.log("Disconnected websocket!")
+        }
+
+        const client = new Client({
+            brokerURL: SOCKET_URL,
+            reconnectDelay: 5000,
+            heartbeatIncoming: 4000,
+            heartbeatOutgoing: 4000,
+            onConnect: onConnected,
+            onDisconnect: onDisconnected
+        });
+
+        client.activate();
+    }, [])
+
+    return (
+        <div className="App">
+            <MessageCreator/>
+            <ul>
+                {messages.map((m, index) => (
+                    <li key={index}>{m}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/components/MessageCreator.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+const MessageCreator = () => {
+    const [formData, setFormData] = useState({
+        message: ''
+    })
+
+    const handleInput = (e) => {
+        setFormData(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:9000/send', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {'Content-Type': 'application/json'}
+        })
+    }
+
+    return (
+        <form onSubmit={sendMessage}>
+            <input onChange={handleInput} placeholder="Enter message" name="message" type="text"/>
+            <button>Send</button>
+        </form>
+    );
+};
+
+export default MessageCreator;
